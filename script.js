11d89130330b9bc2775d608be2aa39de010ce624
Paulo Borges <victorborges296@hotmail.com>
2024-03-20 16:26:16 -0300

    construindo a lógica de pesquisa

--- script.js
+++ script.js
@@ -1,42 +1,55 @@
-const containerVideos = document.querySelector('.videos__container');
-const barraDePesquisa = document.querySelector('.pesquisar__input');
-const videos = document.querySelectorAll('.videos__item');
+const containerVideos = document.querySelector(".videos__container");
 
-async function mostraVideos() {
-    try {
+async function buscarEMostrarVideos(){
+    try{
         const busca = await fetch("http://localhost:3000/videos");
         const videos = await busca.json();
-        
-        videos.forEach((video) => {
-            if(video.categoria == "") {
-                throw new Error("Vídeo não tem categoria");
+
+            videos.forEach((video)=> {
+                if(video.categoria == ""){
+                    throw new Error('Vídeo não tem categoria');
+                }
+                containerVideos.innerHTML += `
+                <li class="videos__item">
+                    <iframe src="${video.url}" title="${video.titulo}" frameborder="0" allowfullscreen></iframe>
+                    <div class="descricao-video">
+                        <img class="img-canal" src="${video.imagem} alt="Logo do Canal">
+                        <h3 class="titulo-video">${video.titulo}</h3>
+                        <p class="titulo-canal">${video.descricao}</p>
+                    </div>
+                </li>
+                `;
+            })
+    } catch(error){
+        containerVideos.innerHTML = `<p> Houve um erro ao carregar os vídeos: ${error}</p>`
+    }
+}
+
+
+buscarEMostrarVideos();
+
+//Código omitido 
+
+//Código omitido
+
+const barraDePesquisa = document.querySelector(".pesquisar__input");
+
+barraDePesquisa.addEventListener("input", filtrarPesquisa);
+
+function filtrarPesquisa(){
+    const videos = document.querySelectorAll(".videos__item");
+
+    if(barraDePesquisa.value != ""){
+        for(let video of videos){
+            let titulo = video.querySelector(".titulo-video").textContent.toLowerCase();
+            let valorFiltro = barraDePesquisa.value.toLowerCase();
+
+            if(!titulo.includes(valorFiltro)){
+                video.style.display = "none";
+            } else {
+                video.style.display = "block";
             }
-            containerVideos.innerHTML += `
-            <li class="videos__item">
-                <iframe src="${video.url}" title="${video.titulo}" frameborder="0" allowfullscreen></iframe>
-                <div class="descricao-video">
-                    <img class="img-canal" src="${video.imagem}" alt="Logo do Canal">
-                    <h3 class="titulo-video">${video.titulo}</h3>
-                    <p class="titulo-canal">${video.descricao}</p>
-                </div>
-            </li>
-            `
-        })
-    } catch(erro) {
-        containerVideos.innerHTML = `
-        <p>Erro ao carregar os vídeos: ${erro}</p>
-        `
+
+        }
     }
 }
-
-function filtrarPesquisa() {
-    if(barraDePesquisa.value != "") {
-        //código aqui
-    } else {
-        //código aqui
-    }
-}
-
-barraDePesquisa.addEventListener('input', filtrarPesquisa);
-
-mostraVideos();
